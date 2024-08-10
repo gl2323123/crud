3ae0988f57dd999eacdbfb6a67add41189732bd7
GL~ <gleblobacev07@gmail.com>
2024-08-11 01:04:56 +0300

    Commit2

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'products',
+        'NAME': 'product',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
